d4587242b1cd10f21e29e2786c8743e3b1037be5
Marcio Rodrigues Mafideju <mafideju@outlook.com>
2018-12-04 12:18:41 -0200

    Socket.IO working

--- server/server.js
+++ server/server.js
@@ -1,27 +1,38 @@
 const express = require('express');
+const socketIO = require('socket.io');
+
 const path = require('path');
 const fs = require('fs');
+const http = require('http');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 const port = process.env.PORT || 7001;
 
 const publicPath = path.join(__dirname, '../public');
 
+app.use(express.static(publicPath));
+
 // MIDDLEWARE PARA REGISTRO DE LOGS
 app.use((req, res, next) => {
   const now = new Date().toString();
-  const log = `${now}: ${req.method} ${req.url}`
+  const log = `${now}: ${req.method} ${req.url}`;
   fs.appendFile('server.log', log + '\n', (err) => {
     if (err) {
-      console.log('error', err)
+      console.log('error', err);
     }
   })
   next();
-})
+});
 
-app.use(express.static(publicPath))
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log('DOWN')
+  })
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Servidor Rodando na Porta ${port}`)
 })
